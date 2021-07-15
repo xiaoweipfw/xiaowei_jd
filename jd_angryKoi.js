e91a813ed9a49db4fbe6ee239c3684afa87a1564
程序工厂 <246771270@qq.com>
2021-07-15 17:35:16 +0800

    1.0

--- jd_angryKoi.js
+++ jd_angryKoi.js
@@ -18,7 +18,7 @@ const $ = new Env('愤怒的锦鲤');
 
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
-var kois = process.env.kois ?? "t142171"
+var kois = process.env.kois ?? ""
 let cookiesArr = []
 var packets = [];
 
