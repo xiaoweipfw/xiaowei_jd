270b622f52faebdc06d16a5f1535c63f1ad418cf
程序工厂 <246771270@qq.com>
2021-07-15 15:35:34 +0800

    1.0

--- jd_sign_graphics.js
+++ jd_sign_graphics.js
@@ -4,8 +4,8 @@
 14 10 * * *
 */
 
-const validator = require('./wuzhi04_MyActions_JDJRValidator_Pure.js');
-const Faker=require('./wuzhi04_MyActions_jd_sign_validate.js')
+const validator = require('./JDWXX_JD_JDJRValidator_Pure.js');
+const Faker=require('./JDWXX_JD_jd_sign_validate.js')
 
 const $ = new Env('京东签到图形验证');
 const notify = $.isNode() ? require('./sendNotify') : '';
